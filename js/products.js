09808ef93e03fdb72280af750f319caf1c88e990
jmanuelsanchezz <juanmasanchezcapo@gmail.com>
2021-08-26 10:43:58 -0300

    Entrega 1

--- js/products.js
+++ js/products.js
@@ -1,6 +1,45 @@
+var currentProductsArray = [];
+
+
+function showProducts(){
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let product = currentProductsArray[i];
+
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <small class="text-muted">` + product.soldCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p> 
+                        <h5> ` + product.currency + " " + product.cost + `</h5>
+                    </div>
+                </div>
+            </a>
+            `       
+
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){            
+            currentProductsArray = resultObj.data;
+            showProducts();            
+        }
+    })
 });
+
